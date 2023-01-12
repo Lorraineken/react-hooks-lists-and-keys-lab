41661297c56936951468efde43a3cfa077d53c38
Lorraineken <lorrainekupa@gmail.com>
2023-01-12 18:33:46 +0300

    Incomplete assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const Linktag = links.map((item) => {
+    return (<a href={`#${item}`} key={item}>{item}</a>)
+  })
+
+  return <nav>{/* display an <a> tag for each link here */}
+  {Linktag}
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,21 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
+
+  const projectsName = projects.map((item) => item.name)
+  const projectsAbout = projects.map((item) => item.about)
+  const projectsTech = projects.map((item) => item.technologies)
+  console.log(projectsTech)
+   
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list" >{/* render ProjectItem components here */}
+      <ProjectItem name={projectsName[0]} about={projectsAbout[0]} technologies={projectsTech[0]}/>
+      <ProjectItem name={projectsName[1]} about={projectsAbout[1]} technologies={projectsTech[1]}/>
+      <ProjectItem name={projectsName[2]} about={projectsAbout[2]} technologies={projectsTech[2]}/>
+      </div>
     </div>
   );
 }
